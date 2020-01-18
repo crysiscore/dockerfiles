bb822a35b346beaed4c726a7353d5a097138edfd
Agnaldo Samuel <agnaldosamuel3@gmail.com>
2020-01-18 03:01:53 +0200

    Mozart Q1 2020

--- setenv.sh
+++
@@ -1,2 +0,0 @@
-#export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xms512m -Xmx1024m  -server"
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xms512m -Xmx2048m -XX:PermSize=64m -XX:MaxPermSize=356m -XX:-UseGCOverheadLimit -XX:+UseParallelGC  -server"
---
+++ v1.05/setenv.sh
@@ -0,0 +1,2 @@
+#export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xms512m -Xmx1024m  -server"
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xms3050m -Xmx3050m -XX:PermSize=2056m -XX:MaxPermSize=2056m -XX:-UseGCOverheadLimit -XX:+UseParallelGC  -server"
